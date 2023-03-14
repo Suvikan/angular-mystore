636d38817f824f6389f77d24f8ee2cf972990a1c
Suvikan <sanledell@gmail.com>
2023-03-14 12:03:02 +0200

    router link works between product-list and product-details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'products/:productId', component: ProductDetailsComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    [RouterModule.forRoot(routes)]
+
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
