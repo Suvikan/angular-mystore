869dbd24251b2b5068f055e1c1dfede9893fe786
Suvikan <sanledell@gmail.com>
2023-03-14 12:16:20 +0200

    edit product-details ts

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
+import { Product, products } from '../products';
+
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
+
+  constructor(private route: AppRoutingModule) { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  
 }
